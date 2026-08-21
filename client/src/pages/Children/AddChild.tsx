a67fa84b0dffc1901f712aa0a68faba4fc8f6614
jdennismunene <dennismunene478@gmail.com>
2026-08-21 10:00:55 +0300

    updates

--- client/src/pages/Children/AddChild.tsx
+++ client/src/pages/Children/AddChild.tsx
@@ -184,14 +184,16 @@ const AddChild = () => {
                       <option value="" disabled>
                         Select class
                       </option>
-                      <option value="Class 1">Class 1</option>
-                      <option value="Class 2">Class 2</option>
-                      <option value="Class 3">Class 3</option>
-                      <option value="Class 4">Class 4</option>
-                      <option value="Class 5">Class 5</option>
-                      <option value="Class 6">Class 6</option>
-                      <option value="Class 7">Class 7</option>
-                      <option value="Class 8">Class 8</option>
+                      <option value="PP2">PP1</option>
+                      <option value="PP1">PP2</option>
+                      <option value="Grade 1">Grade 1</option>
+                      <option value="Grade 2">Grade 2</option>
+                      <option value="Grade 3">Grade 3</option>
+                      <option value="Grade 4">Grade 4</option>
+                      <option value="Grade 5">Grade 5</option>
+                      <option value="Grade 6">Grade 6</option>
+                      <option value="Grade 7">Grade 7</option>
+                      <option value="Grade 8">Grade 8</option>
                     </select>
 
                     <BookOpen
